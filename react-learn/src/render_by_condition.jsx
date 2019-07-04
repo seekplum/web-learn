91015a1d4076440ea2aeb230d941d5a62fb75f31
seekplum <1131909224@qq.com>
2019-07-04 18:28:00 +0800

    ADD: 按条件加载

---
+++ react-learn/src/render_by_condition.jsx
@@ -0,0 +1,80 @@
+/**
+ * 
+ * 按条件渲染页面
+ */
+
+function UserGreeting(props) {
+    return <h1>Welcome Back!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+}
+
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+
+    if (isLoggedIn) {
+        return <UserGreeting />
+    }
+
+    return <GuestGreeting />
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = { isLoggedIn: false };
+    }
+
+    handleLoginClick() {
+        this.setState({ isLoggedIn: true });
+    }
+
+    handleLogoutClick() {
+        this.setState({ isLoggedIn: false });
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />
+        }
+
+        return (
+            <div>
+                <button isLoggedIn={isLoggedIn}>
+                    {button}
+                </button>
+                <p><Greeting isLoggedIn={isLoggedIn} /></p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <LoginControl />,
+    document.getElementById('root')
+);
