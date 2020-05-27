c53c7b9e01bac5c4c28ab06eb77fca8f9ffe5e43
seekplum <1131909224@qq.com>
2020-05-27 18:37:17 +0800

    ADD: react-subscribe Fetch

---
+++ react-learn/src/SubscribePage.jsx
@@ -0,0 +1,45 @@
+/** @format */
+
+import React from "react";
+import Axios from "axios";
+import { Fetch } from "react-subscribe";
+
+function SomeComponent({ data, loading, error, reload, statusCode }) {
+  if (loading) {
+    return <div>loading...</div>;
+  }
+  if (error) {
+    return (
+      <div>
+        Error: {error.message}{" "}
+        <a href="#" onClick={reload}>
+          Reload
+        </a>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <p>Status Code: {statusCode}</p>
+      <p>{JSON.stringify(data)}</p>
+    </div>
+  );
+}
+
+const FETCH_OPTION = {
+  method: "POST",
+  headers: {
+    "X-AccessToken": "some_token",
+  },
+  credentials: "include", // Default credentials is 'same-origin' in `Fetch`
+};
+
+export default function SomePage(props) {
+  return (
+    <div>
+      <Fetch url={`http://httpbin.org/get`} type="json" option={FETCH_OPTION}>
+        <SomeComponent />
+      </Fetch>
+    </div>
+  );
+}
--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -2,12 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import EChartsPage from "./EChartsPage";
+import SomePage from "./SubscribePage";
 
 function Index() {
   return (
     <div>
       <div>执行 npm start 查看页面效果!!!</div>
       <EChartsPage />
+      <SomePage />
     </div>
   );
 }
