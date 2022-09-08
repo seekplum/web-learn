142d655e64ec807e0e07164b3aab19401cda700b
seekplum <1131909224@qq.com>
2022-09-08 16:59:53 +0800

    MOD: 订单表统计用户数量

--- node-learn/src/count_user.js
+++ node-learn/src/count_user.js
@@ -2,43 +2,61 @@
 // @Owner: huangjiandong
 
 const moment = require('moment');
-import { TEST_DB } from 'astrDataSources';
+import { TEST_DB, ORDER_DB } from 'astrDataSources';
 
 // const { MongoClient } = require('mongodb');
 // const TEST_DB_URL = 'mongodb://localhost:27017/';
 
-const endOf2020 = moment('2020-12-31').endOf('day');
 const endOf2021 = moment('2021-12-31').endOf('day');
-const endOf2022 = moment('2022-12-31').endOf('day');
+const endOf2022Half = moment('2022-06-30').endOf('day');
 const format = 'YYYY-MM-DD HH:mm:ss';
 
-console.log(
-  'endOf2020:',
-  endOf2020.format(format),
-  'endOf2021:',
-  endOf2021.format(format),
-  'endOf2022:',
-  endOf2022.format(format)
-);
+async function calc(db, orderDB, endOfDay) {
+  const countByUser = await db.collection('user').countDocuments({ created: { $lte: moment(endOfDay).toDate() } });
+  console.log('count by user:', endOfDay.format(format), countByUser);
 
-async function calc(db) {
-  const count2020 = await db.collection('user').countDocuments({ created: { $lte: moment(endOf2020).toDate() } });
-  const count2021 = await db.collection('user').countDocuments({ created: { $lte: moment(endOf2021).toDate() } });
-  const count2022 = await db.collection('user').countDocuments({ created: { $lte: moment(endOf2022).toDate() } });
-  console.log('count2020:', count2020, 'count2021:', count2021, 'count2022:', count2022);
+  const orders = await orderDB
+    .collection('vas_order')
+    .aggregate(
+      [
+        {
+          $match: {
+            order_cycle_start: { $lte: moment(endOfDay).toDate() },
+          },
+        },
+        {
+          $group: {
+            _id: '$nick',
+            count: { $sum: 1 },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            count: { $sum: 1 },
+          },
+        },
+      ],
+      { allowDiskUse: true }
+    )
+    .toArray();
+  console.log('count by order:', endOfDay.format(format), orders[0]['count']);
 }
 
 async function work() {
-//   const testDB = await MongoClient.connect(TEST_DB_URL, {
-//     poolSize: 1,
-//     useUnifiedTopology: true,
-//   });
+  // const testDB = await MongoClient.connect(TEST_DB_URL, {
+  //   poolSize: 1,
+  //   useUnifiedTopology: true,
+  // });
 
-//   const TEST_DB = testDB.db('testdb');
+  // const TEST_DB = testDB.db('testdb');
+  // const ORDER_DB = TEST_DB;
 
-  await calc(TEST_DB);
+  await calc(TEST_DB, ORDER_DB, endOf2021);
+  console.log('');
+  await calc(TEST_DB, ORDER_DB, endOf2022Half);
 
-//   testDB.close();
+  // testDB.close();
 }
 
 await work();
