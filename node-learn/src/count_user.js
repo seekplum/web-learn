0d586ea61f5ab45ea98946baf34943be4df52f2e
seekplum <1131909224@qq.com>
2022-11-14 10:49:10 +0800

    MOD: 更新统计用户脚本

--- node-learn/src/count_user.js
+++ node-learn/src/count_user.js
@@ -2,59 +2,66 @@
 // @Owner: huangjiandong
 
 const moment = require('moment');
-import { TEST_DB, ORDER_DB } from 'astrDataSources';
+import { ORDER_DB } from 'astrDataSources';
 
 // const { MongoClient } = require('mongodb');
 // const TEST_DB_URL = 'mongodb://localhost:27017/';
 
-const endOf2021 = moment('2021-12-31').endOf('day');
-const endOf2022Half = moment('2022-06-30').endOf('day');
-const format = 'YYYY-MM-DD HH:mm:ss';
+const endOfDay = moment().endOf('day');
 
-async function calc(db, orderDB, endOfDay) {
-  const countByUser = await db.collection('user').countDocuments({ created: { $lte: moment(endOfDay).toDate() } });
-  console.log('count by user:', endOfDay.format(format), countByUser);
-
-  const orders = await orderDB
-    .collection('vas_order')
-    .aggregate(
-      [
-        {
-          $match: {
-            order_cycle_start: { $lte: moment(endOfDay).toDate() },
+async function calc(orderDB) {
+  const matchs = [
+    {
+      total_pay_fee: { $gt: 0 },
+      order_cycle_start: { $lte: moment(endOfDay).toDate() },
+    },
+    {
+      total_pay_fee: 0,
+      order_cycle_start: { $lte: moment(endOfDay).toDate() },
+    },
+    {
+      order_cycle_start: { $lte: moment(endOfDay).toDate() },
+    },
+  ];
+  for (const match of matchs) {
+    const orders = await orderDB
+      .collection('vas_order')
+      .aggregate(
+        [
+          {
+            $match: match,
           },
-        },
-        {
-          $group: {
-            _id: '$nick',
-            count: { $sum: 1 },
+          {
+            $group: {
+              _id: '$nick',
+              count: { $sum: 1 },
+            },
           },
-        },
-        {
-          $group: {
-            _id: null,
-            count: { $sum: 1 },
+          {
+            $group: {
+              _id: null,
+              count: { $sum: 1 },
+            },
           },
-        },
-      ],
-      { allowDiskUse: true }
-    )
-    .toArray();
-  console.log('count by order:', endOfDay.format(format), orders[0]['count']);
+        ],
+        { allowDiskUse: true }
+      )
+      .toArray();
+    const count = orders && orders.length > 0 ? orders[0]['count'] : 0;
+    console.log(JSON.stringify(match), 'count by order:', count);
+  }
 }
 
 async function work() {
   // const testDB = await MongoClient.connect(TEST_DB_URL, {
-  //   poolSize: 1,
+  //   // poolSize: 1,
   //   useUnifiedTopology: true,
   // });
 
   // const TEST_DB = testDB.db('testdb');
   // const ORDER_DB = TEST_DB;
 
-  await calc(TEST_DB, ORDER_DB, endOf2021);
-  console.log('');
-  await calc(TEST_DB, ORDER_DB, endOf2022Half);
+  await calc(ORDER_DB);
 
   // testDB.close();
 }
